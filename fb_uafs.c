8f4bbaf8795d0d09529d94e7f76e6c7f24da267a
Derrick Brashear <shadow@dementix.org>
2011-11-29 23:22:38 -0500

    update fb_uafs copyright to reflect authorship

--- fb_uafs.c
+++ fb_uafs.c
@@ -19,9 +19,10 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2011 Your File System, Inc. All rights reserved.
  * Use is subject to license terms.
  *
+ * Portions Copyright 2009 Sun Microsystems, Inc.
  * Portions Copyright 2008 Denis Cheng
  */
 
